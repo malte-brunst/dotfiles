4c1febe648034561282d0c8858573c2375de8b17
Malte Brunst <malte.brunst@inovex.de>
2025-09-13 15:36:37 +0200

    Backup

--- private_dot_config/private_nvim/private_lua/private_plugins/private_core.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/private_core.lua
@@ -1,10 +1,8 @@
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#f38ba8", bg = "black" })
-
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
 }
